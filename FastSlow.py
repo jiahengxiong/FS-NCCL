678622baedf342c6351d606e565252395c632160
Jiaheng Xiong <1289264578@qq.com>
2025-10-24 15:29:11 +0200

    save topology

--- FastSlow.py
+++ FastSlow.py
@@ -566,8 +566,9 @@ def fastslow(packet_size, pro=0.000001):
         ring = update_ring_edges_from_G(G, ring)
         # ring = reconfigure_WAN(G, ring, weight_attr="total_delay")
         if c%10==0:
-            ring = reconfigure_CCL(G, ring)
-            ring = reconfigure_WAN(G, ring)
+            # ring = reconfigure_CCL(G, ring)
+            # ring = reconfigure_WAN(G, ring)
+            ring = build_ring(G, wan_policy="edge-disjoint", weight_attr="total_delay")
             ring = update_ring_edges_from_G(G, ring)
         else:
             ring = update_ring_edges_from_G(G, ring)
